5ba5f55d6cd198784b4a7613803a5b49c0647564
Olga Tkach <tkacholya@gmail.com>
2018-10-18 14:59:11 -0400

    Initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav
+      className="navbar navbar-light mb-5"
+      style={{ backgroundColor: '#13f4c0' }}
+    >
+      <span className="navbar-brand mb-0 h1 mx-auto">LyricFinder</span>
+    </nav>
+  );
+};
+
+export default Navbar;
